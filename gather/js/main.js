ccc3e4ab3c2f434208f6061574c5980c58dafd24
zhouqt <qqitong@163.com>
2018-01-17 01:55:16 +0800

    wechat

--- gather/js/main.js
+++ gather/js/main.js
@@ -11,26 +11,59 @@ if(isAndroid){
 }else{
 	download_address = ios_address;
 }
-//判断是否微信登陆
-function isWeiXin() {
-	var ua = window.navigator.userAgent.toLowerCase();
-	console.log(ua);//mozilla/5.0 (iphone; cpu iphone os 9_1 like mac os x) applewebkit/601.1.46 (khtml, like gecko)version/9.0 mobile/13b143 safari/601.1
-	if (ua.match(/MicroMessenger/i) == 'micromessenger') {
-		return true;
-	} else {
-		return false;
-	}
+var browser = {
+	versions: function () {
+		var u = navigator.userAgent, app = navigator.appVersion;
+		return {   //移动终端浏览器版本信息
+			trident: u.indexOf('Trident') > -1, //IE内核
+			presto: u.indexOf('Presto') > -1, //opera内核
+			webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+			gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+			mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+			ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+			android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+			iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+			iPad: u.indexOf('iPad') > -1, //是否iPad
+			webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+		};
+	}(),
+	language: (navigator.browserLanguage || navigator.language).toLowerCase()
 }
 
+
 $(function () {
-	if(isWeiXin()){
-		$(".yindao").show();
-		console.log(" 是来自微信内置浏览器")
-	}else{
-		// $(".yindao").hide();
-		console.log("不是来自微信内置浏览器")
+	if (browser.versions.mobile) {//判断是否是移动设备打开。browser代码在下面
+		var ua = navigator.userAgent.toLowerCase();//获取判断用的对象
+		if (ua.match(/MicroMessenger/i) == "micromessenger") {
+			//在微信中打开
+			$(".yindao").show();
+			
+		}
+		if (ua.match(/WeiBo/i) == "weibo") {
+			//在新浪微博客户端打开
+			$(".yindao").hide();
+			
+		}
+		if (ua.match(/QQ/i) == "qq") {
+			//在QQ空间打开
+			$(".yindao").show();
+			
+		}
+		if (browser.versions.ios) {
+			//是否在IOS浏览器打开
+			$(".yindao").hide();
+			
+		}
+		if(browser.versions.android){
+			//是否在安卓浏览器打开
+			$(".yindao").hide();
+			
+		}
+	} else {
+		//否则就是PC浏览器打开
+		$(".yindao").hide();
+		
 	}
-	
 	var mySwiper = new Swiper ('.swiper-container', {
 		direction: 'horizontal',
 		loop: true,
